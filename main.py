5694ee47d45358e6bc5aceda4b7719fccb897587
CarManoel <contatolucasmanoel@gmail.com>
2025-11-24 15:58:50 -0400

    Adiciona exemplo de main e exporta models como modulo

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
-def main:
-  return 
-  #chama func principal que roda o programa inteiro
+import models.cardapio as cardapio
+import models.estoque as estoque
+import models.ingredientes as ingredientes
+
+
+def main():
+    data = {"ingredientes": {}, "cardapio": {}}
+
+    # exemplos de uso
+    ingredientes.adiciona_ingrediente(data, "tomate", 10)
+    cardapio.cria_item_cardapio(data, "salada", 12.0, {"tomate": 2})
+    estoque.gera_venda(data, "salada")
+
+
+if __name__ == "__main__":
+    main()
---
+++ models/__init__.py
@@ -0,0 +1,5 @@
+from . import ingredientes
+from . import cardapio
+from . import estoque
+
+__all__ = ["ingredientes", "cardapio", "estoque"]
