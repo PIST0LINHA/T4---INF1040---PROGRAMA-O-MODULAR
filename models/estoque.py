e365949d943f33c95c254d80fa15cf0b0d075b24
Lucas Manoel <87917746+PIST0LINHA@users.noreply.github.com>
2025-11-24 17:41:46 -0400

    tirar gera_venda do models

--- models/estoque.py
+++ models/estoque.py
@@ -1,26 +1,5 @@
 import ingredientes
 
-
-def gera_venda(data, item):
-    item = item.lower()
-    card = data["cardapio"]
-
-    if item not in card:
-        raise ValueError("Item não existe no cardápio.")
-
-    receita = card[item]["ingredientes"]
-
-    for ing, qtd in receita.items():
-        quant = ingredientes.checa_quant_ingrediente(data, ing)
-        if quant is None:
-            raise ValueError(f"Ingrediente '{ing}' não existe.")
-        if quant < qtd:
-            raise ValueError(f"Quantidade insuficiente de '{ing}'.")
-
-    for ing, qtd in receita.items():
-        ingredientes.deduz_ingrediente(data, ing, qtd)
-
-
 def deduz_item(data, nome, quantidade):
     ingredientes.deduz_ingrediente(data, nome, quantidade)
 
